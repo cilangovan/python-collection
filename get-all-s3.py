2d7efee570f18dc33589685090554166e7dc99e9
cilangovan <cilangovan@deloitte.co.uk>
2019-08-09 18:18:22 +0100

    changes for for looping

--- get-all-s3.py
+++ get-all-s3.py
@@ -2,7 +2,7 @@ import boto3
 
 my_session = boto3.session.Session(profile_name="root")
 
-s3_re=my_session.resource(service_name="s3",region_name="us-east-1")
+#s3_re=my_session.resource(service_name="s3",region_name="us-east-1")
 
  # print(s3_re.buckets.all())
 
@@ -13,14 +13,6 @@ s3_re=my_session.resource(service_name="s3",region_name="us-east-1")
    # print(each_bucke_info)
 
 
-# Create an S3 client
-s3 = boto3.client('s3')
+s3_cli=my_session.client(service_name="s3",region_name="us-east-1")
 
-# Call S3 to list current buckets
-response = s3.list_buckets()
-
-# Get a list of all bucket names from the response
-buckets = [bucket['Name'] for bucket in response['Buckets']]
-
-# Print out the bucket list
-print("Bucket List: %s" % buckets)
+print(s3_cli.listbuckets())
