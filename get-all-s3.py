67a3df4355d5dd00e0a623b389e07b0ce913f199
cilangovan <cilangovan@deloitte.co.uk>
2019-08-09 18:22:39 +0100

    changes for for looping

--- get-all-s3.py
+++ get-all-s3.py
@@ -15,4 +15,4 @@ my_session = boto3.session.Session(profile_name="root")
 
 s3_cli=my_session.client(service_name="s3",region_name="us-east-1")
 
-print(s3_cli.listbuckets())
+print(s3_cli.list_buckets())
