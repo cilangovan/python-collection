da159fe00725632557248d1c43feecde97e46127
Cheralathan Ilangovan <itsme@cheralathan.com>
2019-08-09 16:29:03 +0100

    intial-commit

---
+++ get-all-s3.py
@@ -0,0 +1,12 @@
+import boto3
+
+my_session = boto3.session.Session(profile_name="root")
+
+s3_re=my_session.resource(service_name="s3",region_name="us-east-1")
+
+ # print(s3_re.buckets.all())
+
+
+for each_bucke_info in s3_re.all():
+
+    print(each_bucke_info)
