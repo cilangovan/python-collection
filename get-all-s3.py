fa1eb748a78b25ffd6c7b36fd6885a80029915f1
cilangovan <cilangovan@deloitte.co.uk>
2019-08-09 17:58:52 +0100

    changes for for looping

--- get-all-s3.py
+++ get-all-s3.py
@@ -7,6 +7,12 @@ s3_re=my_session.resource(service_name="s3",region_name="us-east-1")
  # print(s3_re.buckets.all())
 
 
-for each_bucke_info in s3_re.all():
+#
+#for each_bucke_info in s3_re.all():
 
-    print(each_bucke_info)
+   # print(each_bucke_info)
+
+
+for bucket in s3.buckets.all():
+    if bucket.name.startswith("myapp-"):
+        print bucket.name
